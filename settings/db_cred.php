65e637ac7d7793fed5abcce59928b58bd59102ea
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-12-07 22:32:25 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 //Database credentials
-define('SERVERNAME','localhost');
-define('USERNAME','root');
-define('PASSWORD','');
+define('SERVERNAME','us-cdbr-east-06.cleardb.net');
+define('USERNAME','b26ec049b1997e');
+define('PASSWORD','1840a415');
 define('DBNAME','arqengineering');
 
 ?>
