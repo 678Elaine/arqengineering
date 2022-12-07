b297337038496cc4de476c31cec8bc707a044970
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-12-07 23:15:26 +0000

    Update admin.php

--- view/admin.php
+++ view/admin.php
@@ -99,7 +99,7 @@
                             </div>             
                             <div class="col-xl-2 col-lg-2 col-md-3">
                                 <div class="header-right-btn f-right d-none d-lg-block">
-					<button> <a href="index.php" class="btn">Logout</a></button>
+				 <a href="index.php" class="btn">Logout</a>
                             </div>
                             
                             <!-- Mobile Menu -->
