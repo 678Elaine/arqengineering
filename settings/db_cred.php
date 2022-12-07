e1224ecba62f41bcf0a7e8854a2c16c1b04f7159
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-12-07 22:47:33 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -3,6 +3,6 @@
 define('SERVERNAME','us-cdbr-east-06.cleardb.net');
 define('USERNAME','b26ec049b1997e');
 define('PASSWORD','1840a415');
-define('DBNAME','arqengineering');
+define('DBNAME','heroku_ed38f657db5fcb5');
 
 ?>
