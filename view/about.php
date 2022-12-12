fb35ab58d106aff217fe8d3dbdd0dc5d2ef76de7
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-12-12 17:03:35 +0000

    Update about.php

--- view/about.php
+++ view/about.php
@@ -50,10 +50,9 @@
                                 </div>
                                 <div class="header-info-right">
                                     <ul class="header-social">    
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                       <li> <a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                       
+                                        <li><a href="https://gh.linkedin.com/in/ebenezer-roberts-18764416"><i class="fab fa-linkedin-in"></i></a></li>
+                                    
                                     </ul>
                                 </div>
                             </div>
